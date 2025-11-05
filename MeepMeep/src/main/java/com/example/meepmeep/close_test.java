e7b670077886bb0c183a51b089482ce9a20b7a52
rimon598 <rimon.as.282@gmail.com>
2025-11-05 13:43:05 +0200

    create autonomous files

--- MeepMeep/src/main/java/com/example/meepmeep/close_test.java
+++ MeepMeep/src/main/java/com/example/meepmeep/close_test.java
@@ -3,12 +3,11 @@ package com.example.meepmeep;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.core.colorscheme.ColorScheme;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class first_test {
+public class close_test {
     public static void main(String[] args) throws InterruptedException {
     MeepMeep meepMeep = new MeepMeep(700);
 
@@ -19,13 +18,14 @@ public class first_test {
             .setDimensions(18,18)
             .build();
 
-    myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(63, 9, Math.toRadians(180)))
+    myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-50, -48, Math.toRadians(45)))
                     .waitSeconds(8)
-            .splineTo(new Vector2d(36,40), Math.toRadians(90))
-            .splineToSplineHeading(new Pose2d(52,15, Math.toRadians(180)), Math.toRadians(-180))
-                    .waitSeconds(8)
-            .splineTo(new Vector2d(12,30), Math.toRadians(90))
-            .splineTo(new Vector2d(12,40), Math.toRadians(90))
+                    .splineTo(new Vector2d(-12,-30) ,Math.toRadians(-90))
+            .splineTo(new Vector2d(-12,-40) ,Math.toRadians(-90))
+
+
+
+            //.waitSeconds(8)
             .build());
 
     meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
