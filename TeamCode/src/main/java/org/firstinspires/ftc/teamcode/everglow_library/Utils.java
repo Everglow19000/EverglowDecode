3374dc75bceb99abf04d2452d13e903108998de3
Electrocaruzo <gongrossman@gmail.com>
2025-12-05 18:11:38 +0200

    added util method to change servo pwm range

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/everglow_library/Utils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/everglow_library/Utils.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.everglow_library;
 
 import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.hardware.lynx.LynxServoController;
+import com.qualcomm.robotcore.hardware.PwmControl;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Utils {
     public static Vector2d rotateByAngle(Vector2d vector, double angle) {
@@ -8,5 +11,10 @@ public class Utils {
         double newY = vector.x*Math.sin(angle) + vector.y*Math.cos(angle);
 
         return new Vector2d(newX, newY);
+    }
+
+    public static void setServoPWMRange(Servo servo, double usPulseLower, double usPulseUpper) {
+        LynxServoController lynxServoController = (LynxServoController) servo.getController();
+        lynxServoController.setServoPwmRange(servo.getPortNumber(), new PwmControl.PwmRange(usPulseLower, usPulseUpper));
     }
 }
