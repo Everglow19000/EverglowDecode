d85e40459bf48c061a8c4ee031bcc65fcd798355
Electrocaruzo <gongrossman@gmail.com>
2025-12-07 23:27:35 +0200

    added motif artifact colors, none option, and spindexer positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FeedingMechanism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FeedingMechanism.java
@@ -9,15 +9,41 @@ import org.apache.commons.math3.stat.inference.TTest;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.everglow_library.Subsystem;
+import org.firstinspires.ftc.teamcode.everglow_library.Utils;
 import org.opencv.core.Mat;
 
 public class FeedingMechanism implements Subsystem {
+    enum SpindexerPositions {
+        SHOOT_INDEX_0(0.595),
+        SHOOT_INDEX_1(1),
+        SHOOT_INDEX_2(0.205),
+        INTAKE_INDEX_0(0.02),
+        INTAKE_INDEX_1(0.41),
+        INTAKE_INDEX_2(0.79);
+
+        public double position;
+
+        private SpindexerPositions(double position) {
+            this.position = position;
+        }
+    }
+    // servo that pushes artifacts into the shooter
     Servo feedingServo;
+    // servo that rotates the artifact in the spindexer
+    Servo spindexerServo;
     ColorRangeSensor colorSensor;
+    Motif motif;
     ArtifactColor[] storedArtifacts = new ArtifactColor[3];
-    public FeedingMechanism(HardwareMap hardwareMap) {
+    public FeedingMechanism(HardwareMap hardwareMap, Motif motif) {
         this.feedingServo = hardwareMap.get(Servo.class,"feedingServo");
-        this.colorSensor  = hardwareMap.get(ColorRangeSensor.class,"feedingSensor");
+        this.spindexerServo = hardwareMap.get(Servo.class, "spindexerServo");
+        Utils.setServoPWMRange(spindexerServo, 510, 2490);
+        this.colorSensor  = hardwareMap.get(ColorRangeSensor.class,"intakeSensor");
+        this.motif = motif;
+    }
+
+    public void setMotif(Motif motif) {
+        this.motif = motif;
     }
 
     private final int voidThreshold = 3;
